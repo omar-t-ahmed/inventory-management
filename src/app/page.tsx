24e2ce2efc68931f6f050e25352fd23645017c3e
omar-t-ahmed <omartahmed@gmail.com>
2024-08-01 17:01:39 -0400

    add inventory func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,114 @@
-import Image from "next/image";
+'use client'
+import { Box, Modal, Stack, Typography } from "@mui/material";
+import { firestore } from "../../firebase";
+import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+
+interface InventoryItem {
+  name: string;
+}
 
 export default function Home() {
+  const [inventory, setInventory] = useState<InventoryItem[]>([]);
+  const [open, setOpen] = useState<Boolean>(true)
+  const [itemName, setItemName] = useState()
+
+  const handleOpen = () => {setOpen(true)}
+  const handleClose = () => {setOpen(false)}
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList: InventoryItem[] = []
+
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name:doc.id, 
+        ...doc.data()
+      })
+    })
+    setInventory(inventoryList)
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory', item))
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data()
+
+      if (quantity === 1) {
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, {quantiy: quantity - 1})
+      }
+    }
+
+    await updateInventory()
+  }
+
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory', item))
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity + 1})
+    } else {
+      await setDoc(docRef, {quantity: 1})
+    }
+
+    await updateInventory() 
+  }
+
+  useEffect(() => {
+    updateInventory()
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box width="100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+      <Modal open={open} onClose={handleClose}>
+          <Box position={'absolute'} top={'50%'} left={'50%'} width={400} bgcolor={'white'} border={'2px solid #0000'} boxShadow={34} p={4} dosplay={'flex'} flexDirection={'column'} gap ={3} sx={{transform:'translate(-50%,-50%)'}}>
+            <Typography variant="h6" color="#333" textAlign="center">
+              Add Item
+            </Typography>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Stack width='100%' direction={'row'} spacing={2}>
+            </Stack>
+          </Box>
+      </Modal>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Typography variant="h3" color="#333" textAlign="center">
+        Inventory Management
+      </Typography>
+    </Box>
   );
 }
+
+      {/* <Box padding="10px" borderRadius="4px" boxShadow="0 1px 3px rgba(0, 0, 0, 0.5)">
+        <Box width="500px" bgcolor="#9fd4a3" borderRadius="4px" padding={'20px'} boxShadow="0 1px 3px rgba(0, 0, 0, 0.1)" display="flex" justifyContent="center" alignItems="center">
+          <Typography variant="h4" color="#333" textAlign="center">
+            Inventory Management
+          </Typography>
+        </Box>
+        <Stack width="500px" height="400px" spacing={2} overflow="auto">
+          {inventory.map((item, i) => (
+            <Box
+              key={i}
+              width="100%"
+              height="50px"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              bgcolor="#d3d3d3"
+              borderRadius="4px"
+              boxShadow="0 1px 3px rgba(0, 0, 0, 0.1)"
+              p={1}
+            >
+              {item.name}
+              {item.quantity}
+            </Box>
+          ))}
+        </Stack>
+      </Box> */}
